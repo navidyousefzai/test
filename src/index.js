2187e8fb9d6d32e30a767a653b06318d05fb5e03
navidyousefzai <32468790+navidyousefzai@users.noreply.github.com>
2019-11-05 09:19:55 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  const renderNum = () => {
+    return <div>what is goin on </div>;
+  };
+
+  return (
+    <div className="App">
+      <h1>What is going on </h1>
+      <h1>this is the second one</h1>
+      {renderNum()}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
